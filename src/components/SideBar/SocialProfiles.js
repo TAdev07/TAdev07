32efe7af58e3c6f51ed5331efbe5ff89a6ee581f
TAdev07 <tuananh183461@gmail.com>
2018-12-04 11:55:50 +0700

    add effect icon socials

--- src/components/SideBar/SocialProfiles.js
+++ src/components/SideBar/SocialProfiles.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import './SocialEffect.css';
 
 export default function SocialProfiles({dataSocials}) {
-    return (
-        <div>
-            <h5 className="tittle">Social Profiles</h5>
-            <div className="social-icon bor-btm padding-25">
-                <ul>
-                    {dataSocials.map((item, index) => (
-                        <li key={index}>
-                            <a href={item.link}>
-                                <i className={item.icon} />
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h5 className="tittle">Social Profiles</h5>
+      <div className="social-wrapper">
+        <ul className="wrapperIcon">
+          {dataSocials.map((item, index) => (
+            <li key={index} className={item.title}>
+              <a href={item.link}>
+                <span />
+                <span />
+                <span />
+                <span />
+                <span className={item.icon} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/dataLocal/DataSocials.js
+++ src/dataLocal/DataSocials.js
@@ -1,24 +1,29 @@
 const DataSocials = [
-    {
-        icon: 'fa fa-facebook',
-        link: 'https://www.facebook.com/',
-    },
-    {
-        icon: 'fa fa-google',
-        link: 'https://mail.google.com',
-    },
-    {
-        icon: 'fa fa-twitter',
-        link: 'https://twitter.com/',
-    },
-    {
-        icon: 'fa fa-linkedin',
-        link: 'https://www.linkedin.com/',
-    },
-    {
-        icon: 'fa fa-skype',
-        link: 'https://www.skype.com',
-    },
+  {
+    icon: 'fa fa-facebook',
+    link: 'https://www.facebook.com/',
+    title: 'facebook',
+  },
+  {
+    icon: 'fa fa-google',
+    link: 'https://mail.google.com',
+    title: 'google',
+  },
+  {
+    icon: 'fa fa-twitter',
+    link: 'https://twitter.com/',
+    title: 'twitter',
+  },
+  {
+    icon: 'fa fa-linkedin',
+    link: 'https://www.linkedin.com/',
+    title: 'linkedin',
+  },
+  {
+    icon: 'fa fa-skype',
+    link: 'https://www.skype.com',
+    title: 'skype',
+  },
 ];
 
 export default DataSocials;
